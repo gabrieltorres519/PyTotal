f99bb555e1036cd74173e52402d84130b06c826e
gabrieltorres519 <gabrieltorres9909@gmail.com>
2026-01-12 16:56:27 -0600

    1) Primeros pasos del curso (básico)

---
+++ Seccion1_Dia1_PROGRAMA_UN_CREADOR_DE_NOMBRES/ejercicios/1_print.py
@@ -0,0 +1,9 @@
+print("Primer texto")
+
+print('Segunto texto')
+
+var =   32 
+
+print(f'El valor en la variable es {var}')
+
+print(100*100)
---
+++ Seccion1_Dia1_PROGRAMA_UN_CREADOR_DE_NOMBRES/ejercicios/2_Strings/1_Strings_iniciales.py
@@ -0,0 +1,12 @@
+# Concatenación (+)
+print("Hola" + " Mundo")
+
+# Para tomar caracteres especiales como si fueran texto plano (\)
+print(" Hola \"Mundo\" ")
+print('This isn\'t a number')
+ 
+# Caracter especial de nueva línea (\n)
+print("Hola\nMundo")
+
+# Caracter especial de tabulación (\t 4 espacios)
+print("Hola\tMundo")
