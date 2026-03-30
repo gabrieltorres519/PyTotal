50fa761656aa9fc723954ba3807af88d9a3e86e9
gabrieltorres519 <gabrieltorres9909@gmail.com>
2026-03-30 17:12:09 -0600

    8) Restricción del acceso a las vistas para un usuario deslogueado

--- Seccion16_PROGRAMA_UNA_APLICACION_WEB_DE_TAREAS_PENDIENTES/src/proyecto/base/views.py
+++ Seccion16_PROGRAMA_UNA_APLICACION_WEB_DE_TAREAS_PENDIENTES/src/proyecto/base/views.py
@@ -5,6 +5,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin # Atributos de usuario y restricción del acceso a listas del sitio
 
 
 class Logueo(LoginView): # Al  igual que todas las clases de Django, LoginView viene con varios campos a llenar como variables
@@ -15,13 +16,14 @@ class Logueo(LoginView): # Al  igual que todas las clases de Django, LoginView v
     def get_success_url(self):
         return reverse_lazy('pendientes')
 
+# Todas las clases creadas para las diferentes vistas heredarán primero a LoginRequiredMixin para restringir su acceso
 
-class ListaPendientes(ListView): # Al heredar de la clase ListView y el modelo llamado Tarea, buscará por defecto templates/tarea_list.html para renderizar (epecificarlo en settings.py del proyecto (DIRS))
+class ListaPendientes(LoginRequiredMixin, ListView): # Al heredar de la clase ListView y el modelo llamado Tarea, buscará por defecto templates/tarea_list.html para renderizar (epecificarlo en settings.py del proyecto (DIRS))
     model = Tarea # Modelo/Tabla de la que se tomarán  los registros (trae todos los registros en automático)
     context_object_name = 'tareas' # Cambiamos el nombre para que en los templates los registros no lleguen como object_list por default
 
 
-class DetalleTarea(DetailView):# tarea_detail.html porque es el nombre que se va a generar automáticamente para las vistas de detalle de la clase DetailView (se puede cambiar esta configuración)
+class DetalleTarea(LoginRequiredMixin,DetailView):# tarea_detail.html porque es el nombre que se va a generar automáticamente para las vistas de detalle de la clase DetailView (se puede cambiar esta configuración)
     model = Tarea # Al traer todos los registros en automático, desde el html el objeto '{{object}}' muestra el registro en la bd según se reciba una PK desde la url
                   # El uso de estas cláses del framework Django facilita el crud de esta forma (model = modelo_db)
 
@@ -29,20 +31,20 @@ class DetalleTarea(DetailView):# tarea_detail.html porque es el nombre que se va
     template_name = 'base/tarea.html' # En lugar de buscar tarea_detail.html automáticamente por la clase DetailView
 
 
-class CrearTarea(CreateView):  # La clase CreateView automáticamente busca el sufijo _form.html
+class CrearTarea(LoginRequiredMixin,CreateView):  # La clase CreateView automáticamente busca el sufijo _form.html
     model = Tarea 
     # La clase CreateView ya toma el modelo o tabla y lo convierte en un objeto formulario
     fields = '__all__'
     success_url = reverse_lazy('pendientes') # Redireccionando al home (name en las urls)
 
 
-class EditarTarea(UpdateView):
+class EditarTarea(LoginRequiredMixin,UpdateView):
     model = Tarea
     fields = '__all__'
     success_url = reverse_lazy('pendientes')
 
 
-class EliminarTarea(DeleteView): # La clase de Django busca el archivo _confirm_delete.html
+class EliminarTarea(LoginRequiredMixin,DeleteView): # La clase de Django busca el archivo _confirm_delete.html
     model = Tarea
     context_object_name = 'Tarea'
     success_url = reverse_lazy('pendientes')
