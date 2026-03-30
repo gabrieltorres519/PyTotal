a5783bb572ff7b0575a8da51b2231a6c86d3832c
gabrieltorres519 <gabrieltorres9909@gmail.com>
2026-03-30 17:01:59 -0600

    7) Métodos de login y logout

--- Seccion16_PROGRAMA_UNA_APLICACION_WEB_DE_TAREAS_PENDIENTES/src/proyecto/base/urls.py
+++ Seccion16_PROGRAMA_UNA_APLICACION_WEB_DE_TAREAS_PENDIENTES/src/proyecto/base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ListaPendientes, DetalleTarea, CrearTarea, EditarTarea, EliminarTarea
+from .views import ListaPendientes, DetalleTarea, CrearTarea, EditarTarea, EliminarTarea, Logueo
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [path('', ListaPendientes.as_view(), name='pendientes'),
+               path('login/', Logueo.as_view(), name='login'),
+               path('logout/', LogoutView.as_view(next_page='login'), name='logout'), # No ocupa de una vista html
                path('tarea/<int:pk>', DetalleTarea.as_view(), name='tareas'), # /tarea/número de la tarea en la base de datos (pk--primary key)
                path('crear-tarea/', CrearTarea.as_view(), name='crear-tarea'),
                path('editar-tarea/<int:pk>', EditarTarea.as_view(), name='editar-tarea'),
--- Seccion16_PROGRAMA_UNA_APLICACION_WEB_DE_TAREAS_PENDIENTES/src/proyecto/base/views.py
+++ Seccion16_PROGRAMA_UNA_APLICACION_WEB_DE_TAREAS_PENDIENTES/src/proyecto/base/views.py
@@ -3,7 +3,17 @@ from django.views.generic.list import ListView
 from .models import Tarea
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+
+
+class Logueo(LoginView): # Al  igual que todas las clases de Django, LoginView viene con varios campos a llenar como variables
+    template_name  = "base/login.html"
+    field = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('pendientes')
 
 
 class ListaPendientes(ListView): # Al heredar de la clase ListView y el modelo llamado Tarea, buscará por defecto templates/tarea_list.html para renderizar (epecificarlo en settings.py del proyecto (DIRS))
